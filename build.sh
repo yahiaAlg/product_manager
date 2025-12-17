9bc4dc8943db3b837bb7ef4eddb2735c3d51d9d4
yahiaAlg <yahialinus21alg@gmail.com>
2025-12-17 21:17:28 +0100

    Reorder migration and seeding commands in build script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py seed_products
 python manage.py makemigrations
 python manage.py migrate
+python manage.py seed_products
