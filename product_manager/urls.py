a579947fddcb6d4ee3e03a63476149e2aa7654a2
yahiaAlg <yahialinus21alg@gmail.com>
2025-12-17 21:37:15 +0100

    Update certificate files and configure static URL handling in Django

--- product_manager/urls.py
+++ product_manager/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -12,4 +14,4 @@ urlpatterns = [
         ),
         name="service-worker",
     ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
